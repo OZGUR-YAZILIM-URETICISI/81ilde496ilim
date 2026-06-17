9b6b55f0960391c207f996020455889733cf9afe
OZGUR-YAZILIM-URETICISI <yigitefeegin@gmail.com>
2026-06-17 09:32:59 +0300

    Add files via upload

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-var ILLER = [
+if (typeof ILLER === 'undefined') { var ILLER = [
   {
     id: 1, ad: "Adana", plaka: "01", bolge: "Akdeniz",
     nufus: "2.265.022", yuzolcum: "14.030 km²", slogan: "Güneyin incisi",
@@ -1062,4 +1062,4 @@ var ILLER = [
     ],
     ilceler: ["Merkez","Akçakoca","Cumayeri","Çilimli","Gölyaka","Gümüşova","Kaynaşlı","Yığılca"]
   }
-];
+]; }
